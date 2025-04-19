2fb6146de03a2a01d7edb05a3eab747e55b07561
Alexander Dunaev <alexander.dunaev@gmail.com>
2025-04-19 15:17:35 +0200

    Fixed the translation utility scripts to work no matter from which directory they are called

--- src/compilemessages.py
+++ src/compilemessages.py
@@ -1,3 +1,7 @@
+import os
+import pathlib
 import subprocess
+
+os.chdir(pathlib.Path(__file__).parent)
 
 subprocess.run(['pybabel', 'compile', '--directory=locales', '--domain=bot', '--use-fuzzy'])
--- src/updatemessages.py
+++ src/updatemessages.py
@@ -1,7 +1,11 @@
+import os
+import pathlib
 import subprocess
 
 # Should a new translation language be added, register it using the following command:
 # pybabel init --input-file=locales/bot.pot --output-dir=locales --domain=bot --locale=<language code>
+
+os.chdir(pathlib.Path(__file__).parent)
 
 subprocess.run(['pybabel', 'extract', '--input-dirs=.', '--output-file=locales/bot.pot', '--ignore-dirs=venv', '--no-wrap'])
 
